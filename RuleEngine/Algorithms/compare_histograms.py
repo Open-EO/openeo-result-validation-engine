f643f83b49f04a49c57a8c648c8d336e9f01e837
Simon Schulte <simonschulte@posteo.net>
2019-03-21 17:22:41 +0100

    feat: compare Histograms and Dict results
    - first implementation for comparing histograms,
    openCV calcHist function is used as it should be 40X faster
    - results of image similiarty measures are now stored in a dict to be
    able to retrieve them better

--- RuleEngine/Algorithms/compare_histograms.py
+++ RuleEngine/Algorithms/compare_histograms.py
@@ -1,3 +1,23 @@
+import cv2
+
+OPENCV_METHODS = (
+    ("Correlation", cv2.HISTCMP_CORREL),
+    ("Chi-Squared", cv2.HISTCMP_CHISQR),
+    ("Intersection", cv2.HISTCMP_INTERSECT),
+    ("Hellinger", cv2.HISTCMP_BHATTACHARYYA))
+
+
 def compare_histograms(image_a, image_b):
-    # ToDo: Calculate histogram of image_a and image_b and compare them
-    pass
+    # Source: https://www.pyimagesearch.com/2014/07/14/3-ways-compare-histograms-using-opencv-python/
+    # For some similarity functions a LARGER value indicates higher similarity (Correlation and Intersection).
+    # And for others, a SMALLER value indicates higher similarity (Chi-Squared and Hellinger).
+    hist_a = cv2.calcHist(image_a, [0], None, [256], [0, 256])
+    hist_b = cv2.calcHist(image_b, [0], None, [256], [0, 256])
+    results = {}
+    for (methodName, method) in OPENCV_METHODS:
+        result = cv2.compareHist(hist_a, hist_b, method)
+        results[methodName] = result
+
+    return results
+
+
