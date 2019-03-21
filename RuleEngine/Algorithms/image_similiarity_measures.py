6713a80a8ba8fd72ddbba01a72ca4fe6cc941c48
Simon Schulte <simonschulte@posteo.net>
2019-03-21 17:22:54 +0100

    feat: compare Histograms and Dict results
    - first implementation for comparing histograms,
    openCV calcHist function is used as it should be 40X faster
    - results of image similiarty measures are now stored in a dict to be
    able to retrieve them better

--- RuleEngine/Algorithms/image_similiarity_measures.py
+++ RuleEngine/Algorithms/image_similiarity_measures.py
@@ -3,9 +3,9 @@ import cv2
 
 
 def run_image_similarity_measures(image_a, image_b, threshold):
-    scores = []
-    result = 'passed'
+    check = 'passed'
     difference_image = []
+    result = {}
 
     functions = [compare_mse, compare_nrmse, compare_psnr, compare_ssim]
 
@@ -15,17 +15,20 @@ def run_image_similarity_measures(image_a, image_b, threshold):
             gray_a = cv2.cvtColor(image_a, cv2.COLOR_BGR2GRAY)
             gray_b = cv2.cvtColor(image_b, cv2.COLOR_BGR2GRAY)
             score, difference_image = function(gray_a, gray_b, full=True)
+            result[function.__name__] = score
         else:
             score = function(image_a, image_b)
-            print(function.__name__ + ' ' + str(score))
+            result[function.__name__] = score
 
         # ToDo: Weigh the score with the threshold and judge whether the test passed or not
         # ToDo: Create map of functions and what their return value means
         if score in [0.0, 1.0, float("inf")]:
-            result = 'passed'
+            check = 'passed'
         else:
-            result = 'failed'
-        scores.append((str(function.__name__), score))
+            check = 'failed'
 
-    return scores, result, difference_image
+    result['differenceImage'] = difference_image
+    result['rule'] = check
+
+    return result
 
