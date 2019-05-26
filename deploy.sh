9feb26be4f510e2e95b38e7934ac61b280657388
Simon Schulte <simonschulte@posteo.net>
2019-05-26 16:19:44 +0200

    fix: encryption

--- deploy.sh
+++ deploy.sh
@@ -37,9 +37,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-#openssl aes-256-cbc -K $encrypted_db6b38dbc639_key -iv $encrypted_db6b38dbc639_iv -in secrets.tar.enc -out secrets.tar -d
-openssl aes-256-cbc -K $encrypted_db6b38dbc639_key -iv $encrypted_db6b38dbc639_iv -in secrets_GEE-GEE_SR.tar.enc -out secrets.tar -d
+openssl aes-256-cbc -K $encrypted_db6b38dbc639_key -iv $encrypted_db6b38dbc639_iv -in secrets.tar.enc -out secrets.tar -d
 tar xvf secrets.tar
+rm -rf backendProvider.json
+mv backendProvider-S2-SR.json backendProvider.json
 
 
 echo "Running validation"
