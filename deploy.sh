902cce4441842315fa82471a975eb11efffc4c9a
Simon Schulte <simonschulte@posteo.net>
2019-06-19 14:53:08 +0200

    Fix: Build Pipeline
    - Validation reports should now be published again to the git repository

--- deploy.sh
+++ deploy.sh
@@ -34,10 +34,10 @@ cd ..
 
 # Gets backend credentials
 #rm -rf backendProvider.json
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+#ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+#ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+#ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+#ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 # Encryption of tar file for credentials and github token
 #openssl aes-256-cbc -K $encrypted_db6b38dbc639_key -iv $encrypted_db6b38dbc639_iv -in secrets.tar.enc -out secrets.tar -d
 #tar xvf secrets.tar
@@ -65,7 +65,7 @@ git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+#openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
 chmod 600 deploy_key
 eval $(ssh-agent -s)
 ssh-add deploy_key
